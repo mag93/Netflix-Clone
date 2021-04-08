e325d9ba3bb1bfe6677152ebd79e97375fc4b2a4
mag93 <79552796+mag93@users.noreply.github.com>
2021-04-09 00:04:18 +0200

    Netflix Clone

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import HomeScreen from "./HomeScreen.js";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+}
+from "react-router-dom"
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/">
+             <HomeScreen />
+
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDRTjcwR-XCIXXeT3MzZFahdI3UZ7v812w",
+    authDomain: "netflix-clone-3eaf3.firebaseapp.com",
+    projectId: "netflix-clone-3eaf3",
+    storageBucket: "netflix-clone-3eaf3.appspot.com",
+    messagingSenderId: "80701233511",
+    appId: "1:80701233511:web:ca8f984f65141f97c537c2"
+  };
+  
